5d253494e6c00c6dd3a7a04d35ee63fbfd9b8219
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2024-09-09 19:11:54 -0300

    iniciando projeto back-end

---
+++ src/test/java/vet/ser/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vet.ser.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
