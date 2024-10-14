93a86119a78c8a83be05c2576b40d57ea7b95f71
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2024-10-14 20:11:18 -0300

    criando as validações de POST de veterinário

--- src/main/java/vet/ser/api/veterinario/DadosCadastroVeterinario.java
+++ src/main/java/vet/ser/api/veterinario/DadosCadastroVeterinario.java
@@ -1,12 +1,29 @@
 package vet.ser.api.veterinario;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import vet.ser.api.endereco.DadosEndereco;
 
 public record DadosCadastroVeterinario(
+        @NotBlank
         String nome,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+
+        @NotNull
         Especialidade especialidade,
+
+        @NotNull
+        @Valid
         DadosEndereco endereco
 ) {
 }
--- src/main/java/vet/ser/api/veterinario/VeterinarioController.java
+++ src/main/java/vet/ser/api/veterinario/VeterinarioController.java
@@ -1,5 +1,6 @@
 package vet.ser.api.veterinario;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class VeterinarioController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroVeterinario dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroVeterinario dados) {
         repository.save(new Veterinario(dados));
     }
 }
