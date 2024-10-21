31e3bc23d06d8d571bc61749bdc909d340f618fc
Fabio-Gurgel <fabio.cc.gurgel@gmail.com>
2024-10-21 20:28:05 -0300

    criando validações para o cadastro de paciente

--- src/main/java/vet/ser/api/paciente/PacienteRepository.java
+++ src/main/java/vet/ser/api/paciente/PacienteRepository.java
@@ -3,4 +3,7 @@ package vet.ser.api.paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    boolean existsByTutorEmail(String email);
+
+    boolean existsByTutorCpf(String cpf);
 }
--- src/main/java/vet/ser/api/paciente/PacienteService.java
+++ src/main/java/vet/ser/api/paciente/PacienteService.java
@@ -1,5 +1,6 @@
 package vet.ser.api.paciente;
 
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vet.ser.api.veterinario.DadosCadastroVeterinario;
@@ -12,6 +13,31 @@ public class PacienteService {
     PacienteRepository repository;
 
     public Paciente cadastrar(DadosCadastroPaciente dadosParaCadastro) {
+        this.validarDadosAntesDeCadastrar(dadosParaCadastro);
         return repository.save(new Paciente(dadosParaCadastro));
     };
+
+    public void validarDadosAntesDeCadastrar(DadosCadastroPaciente dadosParaCadastro) {
+        String mensagemDeDadosNulos = "Envie os dados do paciente para concluir o cadastro";
+        if (dadosParaCadastro != null) {
+            this.validarEmail(dadosParaCadastro.tutor().tutorEmail());
+            this.validarCpf(dadosParaCadastro.tutor().tutorCpf());
+        } else {
+            throw new ValidationException(mensagemDeDadosNulos);
+        }
+    }
+
+    private void validarEmail(String email) {
+        String mensagemDeEmailInvalido = "Email já cadastrado em outro paciente";
+        if (repository.existsByTutorEmail(email)) {
+            throw new ValidationException(mensagemDeEmailInvalido);
+        }
+    }
+
+    private void validarCpf(String cpf) {
+        String mensagemDeCpfInvalido = "Cpf já cadastrado em outro paciente";
+        if (repository.existsByTutorCpf(cpf)) {
+            throw new ValidationException(mensagemDeCpfInvalido);
+        }
+    }
 }
